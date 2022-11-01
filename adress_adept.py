b73db5c64af3f900a9e2f06e51d666c08caf303f
grigory2213 <arzamastcev.grigory@yandex.ru>
2022-11-01 14:56:54 +0300

    Version_1_11

--- adress_adept.py
+++ adress_adept.py
@@ -1,34 +1,24 @@
 import pandas as pd
 import requests
 import json
-import datetime
+from dadata import Dadata
 
 
-now = datetime.datetime.now()
-now = now.strftime("%d-%m-%Y")
-print(now)
-
-df_adress = pd.read_excel('dadata/XLSX Worksheet.xlsx')
+df_gerioni = pd.read_excel('/home/grigory/Local/Work/dadata_api/Обозначения Регионы.xlsx')
+df_adress = pd.read_excel('dadata_api/XLSX Worksheet.xlsx')
 df_adress['adress'] = df_adress['adress'].str.lower()
 df_adress['adress'] = df_adress['adress'].str.replace("№","")
 print(df_adress)
 
 def get_full_adress(adress):
     
-    headers = {
-        # Already added when you pass json= but not when you pass data=
-        # 'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Token 8f87c1c11a0ab5134fe621044cfa89b35d082563',
-    }
+    token = "8f87c1c11a0ab5134fe621044cfa89b35d082563"
+    secret = "dd0dc733834a73c3e13006ff12f792737c9954a4"
+    dadata = Dadata(token, secret)
+    result = dadata.clean("address", f'{adress}')
+    print(result)
 
-    json_data = {
-        'query': f'{adress}',
-        'count': 1,
-    }   
-
-    response = requests.post('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address', headers=headers, json=json_data).json()
-    return response
+    return result
 
 
 
@@ -37,23 +27,34 @@ def getting_adress():
     cannnot_find = []
     for i in range(len(df_adress)):
         adress = df_adress["adress"][i]
+        print(adress)
         try:
             result = get_full_adress(adress)
-            with open(f'dadata/{i}.json', 'w') as json_file:
+            print(result)
+            with open(f'dadata_api/{i}.json', 'w') as json_file:
                 json.dump(result, json_file)
             
-            with open(f'dadata/{i}.json', 'r') as f:
+            with open(f'dadata_api/{i}.json', 'r') as f:
                 json_data = json.loads(f.read())
-            df = pd.json_normalize(json_data['suggestions'])
-            df = df[['unrestricted_value', 'data.region_with_type', 'data.postal_code', 'data.country_iso_code', 'data.city','data.settlement', 'data.street', 'data.street_type', 'data.house', 'data.house_type', 'data.settlement', 'data.geo_lat', 'data.geo_lon']]
+            df = pd.json_normalize(json_data)
+            df = df[['region_with_type', 'postal_code', 'country_iso_code', 'city','settlement', 'street', 'street_type', 'house', 'house_type', 'settlement', 'geo_lat', 'geo_lon']]
+            df['Store ID'] = ''
+            df['Store ID']= df_adress['Store ID'][i]
+            df['Name'] = ''
+            df['Name'] = df_adress['Name'][i]
+            #df['Phone'][i] = df_adress['Phone'][i]
+            
+            #df['Location Hours'][i] = df_adress['Location Hours'][i]
+            
             new_adress_data = new_adress_data.append(df)
+            
         except:
             print('скип')
             cannnot_find.append(str(adress))
             
             
     
-    new_adress_data.to_excel('dadata/new_adress.xlsx')
+    new_adress_data.to_excel('dadata_api/new_adress.xlsx')
     print("Не смог найти: ", len(cannnot_find))
     print(cannnot_find)
     
@@ -63,25 +64,27 @@ def getting_adress():
     print(df.info())
     return result
 
-getting_adress()
+#getting_adress()
 
-new = pd.read_excel('dadata/new_adress.xlsx')
+new = pd.read_excel('dadata_api/new_adress.xlsx')
 
-sm = pd.read_excel('dadata/Shopmetrics.xlsx')
+sm = pd.read_excel('dadata_api/Shopmetrics.xlsx')
 
-print(new['data.city'])
-print(new['data.settlement'])
+sm['Store ID'] = new['Store ID']
+sm['Location Name'] = new['Name']
+sm['Address'] = new['street_type'] + '.' + ' ' + new['street'] + ',' + ' '  + new['house']
+sm['City'] = new['city']
+sm['Country'] = new['country_iso_code']
+sm['Postal Code'] = new['postal_code']
+sm['Latitude'] = new['geo_lat']
+sm['Longitude'] = new['geo_lon']
+#sm['Phone'] = new['Phone']
+#sm['Location Hours'] = new['Location Hours']
 
-sm['Location Name'] = new['data.region_with_type']
-sm['Address'] = new['data.street_type'] + '.' + ' ' + new['data.street'] + ',' + ' ' + new['data.house_type'] + '.' + ' ' + new['data.house']
-sm['City'] = new['data.city']
-sm['Country'] = new['data.country_iso_code']
-sm['Postal Code'] = new['data.postal_code']
-sm['Latitude'] = new['data.geo_lat']
-sm['Longitude'] = new['data.geo_lon']
-for i in range(len(new)):
-    sm['Store ID'][i] = '101'+ str(now) + str(i)
+sm['Address']  = sm['Address'].str.replace('ш. ', 'шоссе ')
 
+sm = pd.merge(sm, df_gerioni, how = 'left', left_on = 'City', right_on = 'город')
+sm = sm.drop(['город'], axis = 1)
 
 print(sm)
 sm.to_excel('new_adress1.xlsx')
